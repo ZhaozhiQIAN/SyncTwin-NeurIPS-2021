190fe8fd5f8bdb8992c032259158dc15418fbfc6
Evgeny Saveliev <e.s.saveliev@gmail.com>
2021-11-26 04:09:54 +0000

    Direct reproduce_all.sh output to log files

--- experiments/reproduce_all.sh
+++ experiments/reproduce_all.sh
@@ -8,16 +8,16 @@ set +x
 
 
 printf "\n=============== Runnung: Tab6.sh ===============\n\n"
-bash experiments/Tab6.sh
+bash experiments/Tab6.sh > results/Tab6.log 2>&1
 
 printf "\n=============== Runnung: Fig3.sh ===============\n\n"
-bash experiments/Fig3.sh
+bash experiments/Fig3.sh > results/Fig3.log 2>&1
 
 printf "\n=============== Runnung: Tab2.sh ===============\n\n"
-bash experiments/Tab2.sh
+bash experiments/Tab2.sh > results/Tab2.log 2>&1
 
 printf "\n=============== Runnung: Tab5.sh ===============\n\n"
-bash experiments/Tab5.sh
+bash experiments/Tab5.sh > results/Tab5.log 2>&1
 
 printf "\n=============== Runnung: Tab7.sh ===============\n\n"
-bash experiments/Tab7.sh
+bash experiments/Tab7.sh > results/Tab7.log 2>&1
